626489e11ed3e74e0d51fe7b49959af4b8ebe363
luckstar77 <luckstar77y@gmail.com>
2019-11-18 10:38:02 +0800

    init page

---
+++ precache-manifest.eb4813dd1c64d32dd25e4d18f4418a0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d71776225f5f1e80261bce70fdfdf787",
+    "url": "/home/index.html"
+  },
+  {
+    "revision": "4a2d7215ac89ede5c897",
+    "url": "/home/static/css/2.0f87f62a.chunk.css"
+  },
+  {
+    "revision": "d658be92883212ea3ed8",
+    "url": "/home/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4a2d7215ac89ede5c897",
+    "url": "/home/static/js/2.0766520c.chunk.js"
+  },
+  {
+    "revision": "d658be92883212ea3ed8",
+    "url": "/home/static/js/main.1bb2e354.chunk.js"
+  },
+  {
+    "revision": "c33eec9010366509e3c1",
+    "url": "/home/static/js/runtime-main.2fbe4cba.js"
+  }
+]);
