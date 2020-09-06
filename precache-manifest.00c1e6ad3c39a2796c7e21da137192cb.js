a6c30e6ae7114bb6da740a1776a810f214cbe195
raywu <wuhaoshiang1992@gmail.com>
2020-09-06 12:41:16 +0800

    yarn build

---
+++ precache-manifest.00c1e6ad3c39a2796c7e21da137192cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "953e01c3b48e05b43797c29c607642b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dfe4bb1f40ba0547b52e",
+    "url": "/static/css/2.0f87f62a.chunk.css"
+  },
+  {
+    "revision": "2d84ca3f41bb96b4418e",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "dfe4bb1f40ba0547b52e",
+    "url": "/static/js/2.bc49b72c.chunk.js"
+  },
+  {
+    "revision": "2d84ca3f41bb96b4418e",
+    "url": "/static/js/main.2bd34129.chunk.js"
+  },
+  {
+    "revision": "66ec99c66fc63932591b",
+    "url": "/static/js/runtime-main.0cc5ba3c.js"
+  }
+]);
